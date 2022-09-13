a052b2b8ac0692770856a32724193364710a5c56
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-13 11:48:29 -0500

    Ejercicio Formulario

--- llegada.js
+++ llegada.js
@@ -1,5 +1,7 @@
+
+
 let mostrar = async()=>{
-    let peticion = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    let peticion = await fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=boolean");
     let dato = await peticion.json();
     return dato;
     
